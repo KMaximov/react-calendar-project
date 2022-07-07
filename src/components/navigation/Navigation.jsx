a28fa8644ae9454e5b51150607d73cbe3cb4b08d
Mykola <k.maximov19@gmail.com>
2022-07-07 21:13:43 +0300

    docs: display currently week

--- src/components/navigation/Navigation.jsx
+++ src/components/navigation/Navigation.jsx
@@ -1,16 +1,29 @@
 import React from 'react';
-
+import './navigation.scss';
 import { days } from '../../utils/dateUtils.js';
 
 const Navigation = ({ weekDates }) => {
   return (
     <header className="calendar__header">
-      {weekDates.map((dayDate) => (
-        <div className="calendar__day-label day-label">
-          <span className="day-label__day-name">{days[dayDate.getDay()]}</span>
-          <span className="day-label__day-number">{dayDate.getDate()}</span>
+      {weekDates.map((dayDate) => {
+        const currentDay = dayDate.getDate() === new Date().getDate()
+          && dayDate.getMonth() === new Date().getMonth()
+          && dayDate.getFullYear() === new Date().getFullYear()
+          ? 'day-label__day-number_today'
+          : 'day-label__day-number';
+
+        const currentDayName = dayDate.getDate() === new Date().getDate()
+        && dayDate.getMonth() === new Date().getMonth()
+        && dayDate.getFullYear() === new Date().getFullYear()
+        ? 'day-label__day-name_today'
+        : 'day-label__day-name';
+
+        return (
+        <div key={dayDate} className="calendar__day-label day-label">
+          <span className={currentDayName}>{days[dayDate.getDay()]}</span>
+          <span className={currentDay}>{dayDate.getDate()}</span>
         </div>
-      ))}
+      )})}
     </header>
   );
 };
