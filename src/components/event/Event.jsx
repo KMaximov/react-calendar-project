889243a1838c647dd9a03bdcf88f127fefeb1a0d
Mykola <k.maximov19@gmail.com>
2022-07-26 17:07:53 +0300

    docs: show description and delete event

--- src/components/event/Event.jsx
+++ src/components/event/Event.jsx
@@ -1,18 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import './event.scss';
 
-const Event = ({ height, marginTop, title, time }) => {
+const Event = ({ height, marginTop, title, time, description }) => {
   const eventStyle = {
     height,
     marginTop,
   };
 
+  const [eventVisibility, setEventVisibility] = useState({
+    showDeleteBtn: false,
+    showEvent: true,
+  })
+
+  const showEvent = () => {
+    setEventVisibility({
+      showDeleteBtn: !eventVisibility.showDeleteBtn,
+      showEvent: true,
+    })
+  }
+
+  const deleteEvent = (e) => {
+    e.stopPropagation();
+    setEventVisibility({
+      showDeleteBtn: false,
+      showEvent: false,
+    })
+  }
+
   return (
-    <div style={eventStyle} className="event">
-      <div className="event__title">{title}</div>
-      <div className="event__time">{time}</div>
-    </div>
+    <>
+      {eventVisibility.showEvent && (
+        <>
+          <div style={eventStyle} className="event" onClick={showEvent}>
+            <div className="event__title">{title}</div>
+            <div className="event__time">{time}</div>
+            {eventVisibility.showDeleteBtn && (
+              <>
+              <div className="event__description">{description}</div>
+              <button className="delete__event-btn">
+                <i className="fas fa-trash-alt" onClick={deleteEvent}>Delete</i>
+              </button>
+              </>
+            )}
+          </div>
+        </>
+      )}
+    </>
   );
 };
 
